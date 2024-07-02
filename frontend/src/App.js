c6ca59b294895d5115ebdd9bb1b2677b8598b009
Jaroslav Rusanov <sco.nuff@gmail.com>
2024-07-02 15:41:57 +0300

    config deploy on Render 03

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,5 @@
-// import path from 'path';
-// import AutoLoad from '@fastify/autoload';
-// import fastifyStatic from '@fastify/static';
 import logo from './logo.svg';
 import './App.css';
-
-// module.exports = async function (fastify, opts) {
-//   // Place here your custom code!
-
-//   fastify.register(fastifyStatic, {
-//     root: `${process.cwd()}/frontend/build`,
-//   });
-
-//   fastify.setNotFoundHandler((req, res) => {
-//     res.sendFile('index.html');
-//   });
-
-//   // Do not touch the following lines
-
-//   // This loads all plugins defined in plugins
-//   // those should be support plugins that are reused
-//   // through your application
-//   fastify.register(AutoLoad, {
-//     dir: path.join(__dirname, 'plugins'),
-//     options: Object.assign({}, opts)
-//   });
-
-//   // This loads all plugins defined in routes
-//   // define your routes in one of these
-//   fastify.register(AutoLoad, {
-//     dir: path.join(__dirname, 'routes'),
-//     options: Object.assign({}, opts)
-//   });
-// }
 
 function App() {
   return (
