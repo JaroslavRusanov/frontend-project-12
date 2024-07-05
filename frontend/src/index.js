167b7759d63f8abbbbffd295bec7b0b67e9b109f
Jaroslav Rusanov <sco.nuff@gmail.com>
2024-07-05 15:25:22 +0300

    config bootstrap

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-utilities.css';
 import App from './App.js';
 import reportWebVitals from './reportWebVitals.js';
 
