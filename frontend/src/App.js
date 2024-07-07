110816e906a3d6d053ddb62a3a0a65b36ec3d6ef
Jaroslav Rusanov <sco.nuff@gmail.com>
2024-07-07 16:56:05 +0300

    add routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,24 @@
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './components/Login.jsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './components/Login/Login.jsx';
+import NotFound from './components/NotFound/NotFound.jsx';
 
-const App = () => <Login />;
+const App = () => (
+  <div className="h-100 bg-light">
+    <div className="h-100" id="chat">
+      <div className="d-flex flex-column h-100">
+        <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
+          <div className="container"><a className="navbar-brand" href="/">Hexlet Chat</a></div>
+        </nav>
+        <BrowserRouter>
+          <Routes>
+            {}
+            <Route path="*" element={<NotFound />} />
+            <Route path="login" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </div>
+  </div>
+);
 
 export default App;
---
+++ frontend/src/components/NotFound/NotFound.jsx
@@ -0,0 +1,12 @@
+const NotFound = () => (
+  <div className="text-center">
+    <img alt="Страница не найдена" className="img-fluid h-25" src="" />
+    <h1 className="h4 text-muted">Страница не найдена</h1>
+    <p className="text-muted">
+      Но вы можете перейти
+      <a href="/"> на главную страницу</a>
+    </p>
+  </div>
+);
+
+export default NotFound;
